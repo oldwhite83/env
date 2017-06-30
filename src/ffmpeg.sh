7470d4ba8ca4ac98b67b1371ad84cfaeecd65c48
TimJuly <timjuly@163.com>
2017-06-30 09:39:12 +0800

    ffmpeg

--- src/ffmpeg.sh
+++ src/ffmpeg.sh
@@ -7,3 +7,5 @@ fi
 tar Jxf "$BASE_PATH"/source/ffmpeg-release-64bit-static.tar.xz -C /tmp/ffmpeg --strip 1
 yes | cp -f /tmp/ffmpeg/ffmpeg /usr/local/bin/ffmpeg
 yes | cp -f /tmp/ffmpeg/ffprobe /usr/local/bin/ffprobe
+
+rm -rf /tmp/ffmpeg
