88e7d5cfa908b38797814d768af2877b53b95a58
TimJuly <timjuly@163.com>
2017-06-30 10:38:14 +0800

    setuptools

--- src/supervisor.sh
+++ src/supervisor.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # python "$BASE_PATH"/source/get-pip.py
-
+cd "$BASE_PATH"/source/setuptools/
+python setup.py install
 cd "$BASE_PATH"/source/meld3/
 python setup.py install
 cd "$BASE_PATH"/source/supervisor/
 python setup.py install
+
+cd "$BASE_PATH"
 
 if [[ ! -f /etc/supervisord.conf ]]; then
     cp "$BASE_PATH"/ini/supervisor/supervisord.conf /etc/supervisord.conf
