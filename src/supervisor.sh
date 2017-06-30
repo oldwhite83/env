237472a66c2ad6f8921f8fafa730f5481ef8574c
TimJuly <timjuly@163.com>
2017-06-30 10:29:52 +0800

    supervisor offline

--- src/supervisor.sh
+++ src/supervisor.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-python "$BASE_PATH"/source/get-pip.py
+# python "$BASE_PATH"/source/get-pip.py
 
-easy_install supervisor
+cd "$BASE_PATH"/source/meld3/
+python setup.py install
+cd "$BASE_PATH"/source/supervisor/
+python setup.py install
 
 if [[ ! -f /etc/supervisord.conf ]]; then
     cp "$BASE_PATH"/ini/supervisor/supervisord.conf /etc/supervisord.conf
